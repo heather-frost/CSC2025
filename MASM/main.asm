7feb37a859d0a22f0b89ce459a05c03884de017f
samedberry <helk62@gmail.com>
2024-09-18 11:47:58 -0600

    Initial Template
    followed directions in class

---
+++ MASM/main.asm
@@ -0,0 +1,21 @@
+; MASM Template
+; Spencer MedberrY
+; 09/18/2024
+; Create a template for assembler files.
+
+.386P
+.model flat
+
+extern   _ExitProcess@4: near
+
+.data
+
+.code
+main PROC near
+_main:
+
+	push	0
+	call	_ExitProcess@4
+
+main ENDP
+END
