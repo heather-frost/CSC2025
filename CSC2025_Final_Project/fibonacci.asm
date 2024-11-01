407f096fb4a0256cdb7400feb34451ec6f01b671
samedberry <143440194+samedberry@users.noreply.github.com>
2024-11-01 11:05:57 -0600

    Final Project start
    Copied and modified masm into Final Project

---
+++ CSC2025_Final_Project/fibonacci.asm
@@ -0,0 +1,187 @@
+; Main Console program
+; Spencer Medberry
+; 27 September 2024
+; fibonacci counter built from start.asm
+; Revised: SAM 10/12/2024 updated based on feedback
+; Revised: SAM 10/16/2024 added input checking and final dialog
+
+; Register usage:
+;     EAX - readWrite procedure communication, fibonacci sum
+;     EBX - integer user input result for term counting
+;     ECX - helper register: char storage during ASCII conversion, lesser fibonacci addend
+
+.model flat
+
+extern writeline: near
+extern readline: near
+extern charCount: near
+extern writeNumber: near
+
+.data
+
+; both end with 0 to terminate the string
+prompt              byte  "How many fibonacci terms would you like? Enter a number between 1 and 45: ", 0
+; beginning with 10 sends a line feed character before the text
+fibonacciDialog     byte  10,"Starting with 1 and 2, the terms produced are: ",0
+termSizeErrorMsg    byte  "Please enter a term between 1 and 45.",10,10,0
+finalTerm           byte  10,10,"The value of term ",0
+finalDialog         byte  "is "
+
+.code
+
+;; Call fibonacci() - No Parameters, no return value
+fibonacci PROC near
+_fibonacci:
+    ; Display prompt for user input
+        ;; Call charCount(addr)
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;; Returns character count in eax
+    push  offset prompt
+    call  charCount
+        ;; Call writeline(addr, chars) - push parameter in reverse order
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;;             chars is the character count in the buffer
+        ;; Returns nothing
+    push  eax
+    push  offset prompt
+    call  writeline
+
+    ; ask console for user input
+        ;; Call readline() - No Parameters, Returns ptr to buffer in eax
+    call  readline
+    ; user input stored in eax
+    
+    ;convert user input from ASCII to integer
+    mov   ecx,0
+    mov   ebx,0
+ASCIIloop:
+    mov  cl,[eax]                   ; Look at the character in the string
+    cmp  ecx,13                     ; check for carriage return.
+    je numberGet
+    cmp  ecx,10                     ; check for line feed.
+    je numberGet
+    cmp  ecx,0                      ; check for end of string.
+    je numberGet
+    sub  cl,'0'
+    imul ebx,10
+    add  ebx,ecx
+    inc  eax                        ; go to next letter
+    jmp  ASCIIloop
+numberGet:
+    cmp ebx,45
+    jg termSizeError
+    cmp ebx,1
+    jl termSizeError
+    push ebx
+    
+
+    ; Print fibonacciDialog dialog
+        ;; Call charCount(addr)
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;; Returns character count in eax
+    push  offset fibonacciDialog
+    call  charCount
+        ;; Call writeline(addr, chars) - push parameter in reverse order
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;;             chars is the character count in the buffer
+        ;; Returns nothing
+    push  eax
+    push  offset fibonacciDialog
+    call  writeline
+
+    ;prep registers for addloop
+    pop ebx
+    mov   eax, 2
+    mov   ecx, 1
+    push ebx
+
+    ;addloop adds eax and ecx to get the next term in the fibonacci sequence and also prints it to the console
+addloop:
+    ;add function
+    push  eax
+    add   eax, ecx
+    pop   ecx
+
+    ;store registers
+    push  eax
+    push  ecx
+    push  ebx
+
+    ;writeNumber
+        ;; Call writeNumber(number) - print the ASCII value of a number.
+        ;; Parameter: number is number to be converted to Ascii and printed.
+        ;; Returns nothing
+    push  eax
+    call  writeNumber
+
+    ;retrieve registers
+    pop   ebx
+    pop   ecx
+    pop   eax
+
+    ;loopy bit
+    dec ebx
+    cmp ebx,0
+    jg  addloop
+;End addloop
+
+;Ending dialog
+    pop ebx
+    push eax    ;2nd writeNumber parameter
+    push ebx    ;1st writeNumber parameter
+        ;; Call charCount(addr)
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;; Returns character count in eax
+    push  offset finalTerm
+    call  charCount
+        ;; Call writeline(addr, chars) - push parameter in reverse order
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;;             chars is the character count in the buffer
+        ;; Returns nothing
+    push  eax
+    push  offset finalTerm
+    call  writeline
+        ;; Call writeNumber(number) - print the ASCII value of a number.
+        ;; Parameter: number is number to be converted to Ascii and printed.
+        ;; Returns nothing
+    call  writeNumber
+        ;; Call charCount(addr)
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;; Returns character count in eax
+    push  offset finalDialog
+    call  charCount
+        ;; Call writeline(addr, chars) - push parameter in reverse order
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;;             chars is the character count in the buffer
+        ;; Returns nothing
+    push  eax
+    push  offset finalDialog
+    call  writeline
+        ;; Call writeNumber(number) - print the ASCII value of a number.
+        ;; Parameter: number is number to be converted to Ascii and printed.
+        ;; Returns nothing
+    call  writeNumber
+
+
+exit:
+    ret     ; Return to the main program.
+
+termSizeError:
+    ; Print termSizeErrorMsg
+        ;; Call charCount(addr)
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;; Returns character count in eax
+    push  offset termSizeErrorMsg
+    call  charCount
+        ;; Call writeline(addr, chars) - push parameter in reverse order
+        ;; Parameters: addr is address of buffer = &addr[0]
+        ;;             chars is the character count in the buffer
+        ;; Returns nothing
+    push  eax
+    push  offset termSizeErrorMsg
+    call  writeline
+    jmp _fibonacci
+; End termSizeError
+    
+fibonacci ENDP
+END
---
+++ CSC2025_Final_Project/readWrite.asm
@@ -0,0 +1,179 @@
+; Main Console program
+; Wayne Cook
+; 20 September 2024
+; Show how to do input and output
+; Revised: WWC 14 March 2024 Added new module
+; Revised: WWC 15 March 2024 Added this comment ot force a new commit.
+; Revised: WWC 13 September 2024 Minore updates for Fall 2024 semester.
+; Register names:
+; Register names are NOT case sensitive eax and EAX are the same register
+; x86 uses 8 registers. EAX (Extended AX register has 32 bits while AX is
+;	the right most 16 bits of EAX). AL is the right-most 8 bits.
+; Writing into AX or AL effects the right most bits of EAX.
+;     EAX - caller saved register - usually used for communication between
+;			caller and callee.
+;     EBX - Callee saved register
+;     ECX - Caller saved register - Counter register 
+;     EDX - Caller Saved register - data, I use it for saving and restoring
+;			the return address
+;     ESI - Callee Saved register - Source Index
+;     EDI - Callee Saved register - Destination Index
+;     ESP - Callee Saved register - stack pointer
+;     EBP - Callee Saved register - base pointer.386P
+
+.model flat
+
+; Library calls used for input from and output to the console
+extern  _GetStdHandle@4:near
+extern  _WriteConsoleA@20:near
+extern  _ReadConsoleA@20:near
+extern  _ExitProcess@4: near
+
+.data
+
+msg             byte  "Hello, World", 10, 0   ; ends with line feed (10) and NULL
+prompt          byte  "Please type your name: ", 0 ; ends with string terminator (NULL or 0)
+results         byte  10,"You typed: ", 0
+outputHandle    dword ?           ; Output handle writing to consol. uninitslized
+inputHandle     dword ?           ; Input handle reading from consolee. uninitslized
+written         dword ?
+INPUT_FLAG      equ   -10
+OUTPUT_FLAG     equ   -11
+
+; Reading and writing requires buffers. I fill them with 00h.
+readBuffer      byte  1024        DUP(00h)
+writeBuffer     byte  1024        DUP(00h)
+numberBuffer    byte  1024        DUP(00h)
+numCharsToRead  dword 1024
+numCharsRead    dword ?                                   ; Unset or uninitialized
+
+
+.code
+
+;; Call initialize_console() - No Parameters, no return value
+;; Initialize Input and Output handles so you only have to do that once.
+;; This is your first assembly routine
+initialize_console PROC near
+_initialize_console:
+
+    ; handle = GetStdHandle(-11)
+    push    OUTPUT_FLAG
+    call    _GetStdHandle@4
+    mov     outputHandle, eax
+    ; handle = GetStdHandle(-10)
+    push  INPUT_FLAG
+    call  _GetStdHandle@4
+    mov   inputHandle, eax
+    ret
+initialize_console ENDP
+
+;; Call readline() - No Parameters, Returns ptr to buffer in eax
+;; Now the read/write handles are set, read a line
+readline PROC near
+_readline: 
+      ; ReadConsole(handle, &buffer, numCharToRead, numCharsRead, null)
+    push  0
+    push  offset numCharsRead
+    push  numCharsToRead
+    push  offset readBuffer
+    push  inputHandle
+    call  _ReadConsoleA@20
+    mov   eax, offset readBuffer
+    ret
+readline ENDP
+
+;; Call charCount(addr)
+;; Parameters: addr is address of buffer = &addr[0]
+;; Returns character count in eax
+;; All strings need to end with a NULL (0). So I do not have to manually count the number of
+;;   characters in the line, I wrote this routine.
+charCount PROC near
+_charCount:
+    pop  edx                        ; Save return address
+    pop  ebx                        ; save offset/address of string
+    push edx                        ; Put return address back on the stack
+    mov  eax,0                      ; load counter to 0
+    mov  ecx,0                      ; Clear ECX register
+_countLoop:
+    mov  cl,[ebx]                   ; Look at the character in the string
+    cmp  ecx,0                      ; check for end of string.
+    jle  _endCount
+    inc  eax                        ; Up the count by one
+    inc  ebx                        ; go to next letter
+    jmp  _countLoop
+_endCount:
+    ret                             ;Return with EAX containing character count
+charCount ENDP
+
+;; Call writeline(addr, chars) - push parameter in reverse order
+;; Parameters: addr is address of buffer = &addr[0]
+;;             chars is the character count in the buffer
+;; Returns nothing
+;; For all routines, the last item to be pushed on the stack is the return address, save it to a register
+;; then save any other expected parameters in registers, then restore the return address to the stack.
+writeline PROC near
+_writeline:
+    pop   edx                        ; pop return address from the stack into EDX
+    pop   ebx                        ; Pop the buffer location of string to be printed into EBX
+    pop   eax                        ; Pop the buffer size string to be printed into EAX.
+    push  edx                        ; Restore return address to the stack
+
+
+    ; WriteConsole(handle, &msg[0], numCharsToWrite, &written, 0)
+    push   0
+    push   offset written
+    push   eax                       ; return size to the stack for the call to _WriteConsoleA@20 (20 is how many bits are in the call stack)
+    push   ebx                       ; return the offset of the line to be written
+    push   outputHandle
+    call   _WriteConsoleA@20
+    ret
+writeline ENDP
+
+;; Call writeNumber(number) - print the ASCII value of a number.
+;; Parameter: number is number to be converted to Ascii and printed.
+;; Returns nothing
+;; For all routines, the last item to be pushed on the stack is the return address, save it to a register
+;; then save any other expected parameters in registers, then restore the return address to the stack.
+writeNumber PROC near
+_writeNumber:
+    pop   edx                        ; pop return address from the stack into EDX
+    pop   eax                        ; Pop the number to be written.
+    push  edx                        ; Restore return address to the stack
+    push  esi                        ; Preserve for the caller
+    mov   ecx, 10                    ; Set the divisor to ten
+    mov   esi, 0                     ; Count number of numbers written
+    mov   ebx, offset numberBuffer   ; Save the start of the write buffer
+;; The dividend is place in eax, then divide by ecx, the result goes into eax, with the remiander in edx
+genNumLoop:
+    cmp   eax, 0                     ; Stop when the nubmer is 0
+    jle   endNumLoop
+    mov   edx, 0                     ; Clear the register for the remainder
+    div   ecx                        ; Do the divide
+    add   dx,'0'                     ; Turn the remainer into an ASCII number
+    push  dx                         ; Now push the remainder onto the stack
+    inc   esi                        ; increment number count
+    jmp   genNumLoop                 ; One more time.
+endNumLoop:
+    cmp   esi,0
+    jle   numExit
+    pop   dx
+    mov   [ebx], dx                  ; Add the number to the output sring
+    dec   esi                        ; Get ready for the next number
+    inc   ebx                        ; Go to the next character
+    jmp   endNumLoop                 ; Do it one more time
+    
+numExit:
+    mov   dx, ' '                    ; cannot load a literal into an addressed location
+    mov   [ebx], dx                  ; Add a space to the end of the number
+    mov   [ebx+1], esi               ; Add the number to the output sring
+    push  offset numberBuffer
+    call  charCount
+    push  eax
+    push  offset numberBuffer
+    call  writeline
+    pop   esi                        ; Restore for the user.
+    ret
+    
+writeNumber ENDP
+
+END
